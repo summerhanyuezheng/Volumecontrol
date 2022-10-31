404f7eebb307bcf42c58588805c10b0f4a49c166
summerhanyuezheng <summer199303@gmail.com>
2022-10-31 16:58:07 -0700

    it can draw points and connect lines now

--- demo.py
+++ demo.py
@@ -3,12 +3,13 @@ import mediapipe as mp
 
 
 mp_hands = mp.solutions.hands
+draw = mp.solutions.drawing_utils
 #create a hand object
 hands = mp_hands.Hands()
 
 #passed in 1 becoz 1 want to use my second webcam
 #variable capture has all the frames that's captured
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(1)
 while True:
     #variable "image" has each frame
     value,image = capture.read() #a tuple
@@ -16,6 +17,9 @@ while True:
     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     # Print handedness and draw hand landmarks on the image.
     print(results.multi_hand_landmarks)
+    if (results.multi_hand_landmarks):
+        for handlandmarks in results.multi_hand_landmarks:
+            draw.draw_landmarks(image, handlandmarks,mp_hands.HAND_CONNECTIONS) 
     cv2.imshow('Image capture', image)
     # a if statement for "pressing esc to exit the loop"
     if cv2.waitKey(1) & 0xFF == 27:
